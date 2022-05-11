e01fb12bfced2fe3f611a5538043fd1fc918a450
jyuno426 <jyuno426@gmail.com>
2022-05-11 22:19:58 +0900

    skip_before_filter -> skip_before_action

--- app/controllers/remotty/users/sessions_controller.rb
+++ app/controllers/remotty/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Remotty::Users::SessionsController < Devise::SessionsController
   include Remotty::Users::BaseController
   
   wrap_parameters :user, include: [:email, :password]
-  skip_before_filter :verify_signed_out_user, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   # POST /resource/sign_in
   # email과 password로 로그인
